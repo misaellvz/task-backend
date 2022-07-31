b7eb1b22ddffbbb45aefeffdb96c5b7b248a17e3
Elias Misael Valdez Faria <evaldez@cerouno.com.py>
2022-07-31 18:11:46 -0400

    heroku deploy

--- src/config/utils.ts
+++ src/config/utils.ts
@@ -1,7 +1,12 @@
 import { existsSync } from 'fs';
 import { join, resolve } from 'path';
 
+const ENV = process.env.NODE_ENV;
+
 export function getEnvPath(fileName: string): string {
+  if (ENV === 'prod') {
+    return '.env.prod';
+  }
   const dest = join(__dirname, '..', '..');
   const filePath = resolve(`${dest}/${fileName}`);
   if (!existsSync(filePath)) {
